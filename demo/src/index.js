5c5160bb1116a3a4aee46951d628d2863d412926
Alistair Phillips <alphillips101@gmail.com>
2017-07-31 16:33:08 +1000

    Adds menu to header

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,12 +1,28 @@
 import React from 'react'
 import {render} from 'react-dom'
 
-import Component from '../../src'
+import './uikit.css'
+import './base.css'
+
+import Header from '../../src'
+
+
 
 let Demo = React.createClass({
   render() {
-    return <div>
-      <Component/>
+    return <div className="uikit-body">
+      <Header
+        menu={
+          <ul>
+            <li><a href="#/">Home</a></li>
+            <li><a href="#/menu1">Menu 1</a></li>
+            <li><a href="#/menu2">Menu 2</a></li>
+            <li><a href="#/menu3">Menu 3</a></li>
+            <li><a href="#/menu4">Menu 4</a></li>
+          </ul>
+        }
+        name="Heading"
+      />
     </div>
   }
 })
--- src/index.js
+++ src/index.js
@@ -1,20 +1,90 @@
 import React from 'react'
-
+import Menu from 'react-burger-menu/lib/menus/slide'
 import logo from './da-logo.svg'
 import './header.css'
+import './uikit.css'
+import './application-side-menu.css'
+class Header extends React.Component {
 
-const Header = (props) => {
+    constructor(props) {
+        super(props)
+        this.state = {
+          isOpen: false
+        }
+    }
 
-    return (
+    componentDidMount() {
+      this.showMenuContext()
+    }
+
+    onclick = () => {
+      this.setState((prevState, props) => ({
+        isOpen:false
+      }))
+    }
+
+    componentWillReceiveProps = (nextProps) => {
+      this.showMenuContext()
+    }
+
+    showMenuContext = () => {
+      try {
+        // DOM manipulation for showing current manu item
+        let links = document.querySelectorAll('.header nav[class="global-menu"] a')
+        let link = document.querySelector('.header nav[class="global-menu"] a[href="' + document.location.hash + '"]')
+        let i
+        if(links && link){
+          for (i=0;i<links.length;i++){
+            links[i].className = ''
+          }
+          link.className = 'current'
+        }
+    } catch(e){}
+    }
+
+    render() {
+      return (
       <div className="header">
-        <div className="header-block">
-          <div className="header-logo">
-            <a href="http://www.agriculture.gov.au/">
-              <img src={logo} className="header-logo-img" alt="Go to home page" aria-hidden="true" />
-            </a>
+        <div className="side-menu-container">
+          <Menu isOpen={ this.state.isOpen } className="bm-menu" width={ '50%' } onClick={this.onclick}>
+            <div className="side-menu">
+              {this.props.menu}
+            </div>
+          </Menu>
+        </div>
+        <div className="header-block uikit-grid">
+          <div className="container">
+            <div className="row">
+              <div className="header-logo grids col-md-9">
+                <a href="http://www.agriculture.gov.au/">
+                  <img src={logo} className="header-logo-img" alt="Go to home page" aria-hidden="true" />
+                </a>
+              </div>
+              {/*
+              <div className="grids col-md-7">
+                <nav role="navigation" className="global-menu">
+                 {this.props.menu}
+                </nav>
+              </div>
+              */}
+              <div className="grids col-md-3">
+                {/*<a href="#" className="return-to-link uikit-direction-link uikit-direction-link--left">Return to Client Portal</a>*/}
+                <div className="nexdoc-header-name">
+                  {this.props.name}
+                </div>
+              </div>
+            </div>
+            <div className="row">
+              <div className="grids col-md-20">
+                <nav role="navigation" className="global-menu">
+                 {this.props.menu}
+                </nav>
+              </div>
+            </div>
           </div>
         </div>
       </div>
-    )
+     )
+   }
 }
 export default Header
