c04b32e25967172c85b67c5f69c9c3fde0cd47da
Alistair Phillips <alphillips101@gmail.com>
2017-09-14 10:14:57 +1000

    Update to new style

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import Menu from 'react-burger-menu/lib/menus/slide'
+import { Link, hashHistory } from 'react-router'
+
 import logo from './da-logo.svg'
 import './header.css'
 import './uikit.css'
@@ -30,8 +32,8 @@ class Header extends React.Component {
     showMenuContext = () => {
       try {
         // DOM manipulation for showing current manu item
-        let links = document.querySelectorAll('.header nav[class="global-menu"] a')
-        let link = document.querySelector('.header nav[class="global-menu"] a[href="' + document.location.hash + '"]')
+        let links = document.querySelectorAll('nav[class="global-menu"] a')
+        let link = document.querySelector('nav[class="global-menu"] a[href="' + document.location.hash + '"]')
         let i
         if(links && link){
           for (i=0;i<links.length;i++){
@@ -39,52 +41,100 @@ class Header extends React.Component {
           }
           link.className = 'current'
         }
-    } catch(e){}
+      } catch(e){}
     }
 
     render() {
-      return (
-      <div className="header">
-        <div className="side-menu-container">
-          <Menu isOpen={ this.state.isOpen } className="bm-menu" width={ '50%' } onClick={this.onclick}>
-            <div className="side-menu">
-              {this.props.menu}
+
+      if(window.IS_STAFF){
+        return (
+        <div className="staff-header">
+          {/*<div className="side-menu-container">
+            <Menu isOpen={ this.state.isOpen } className="bm-menu" width={ '50%' } onClick={this.onclick}>
+              <div className="side-menu">
+                {this.props.menu}
+              </div>
+            </Menu>
+          </div>*/}
+
+
+          <div className="top-links-wrapper">
+            <div className="top-links">
+              <ul>
+                <li><Link to="/" className="staff-home-link">Home</Link></li>
+                <li><Link to="/">{this.props.userName}</Link></li>
+                <li className="logout-li-link-staff"><a href="/auth/faces/logout/">Logout</a></li>
+              </ul>
             </div>
-          </Menu>
+            </div>
+          <div className="logo">
+            <h1>{this.props.name}</h1>
+            <nav role="navigation" className="global-menu">
+             {this.props.menu}
+            </nav>
+          </div>
+
         </div>
-        <div className="header-block uikit-grid">
-          <div className="container">
-            <div className="row">
-              <div className="header-logo grids col-md-9">
-                <a href="http://www.agriculture.gov.au/">
-                  <img src={logo} className="header-logo-img" alt="Go to home page" aria-hidden="true" />
-                </a>
+       )
+      } else {
+
+        return (
+        <div className="header">
+          <div className="side-menu-container">
+            <Menu isOpen={ this.state.isOpen } className="bm-menu" width={ '50%' } onClick={this.onclick}>
+              <div className="side-menu">
+                {this.props.menu}
               </div>
-              {/*
-              <div className="grids col-md-7">
-                <nav role="navigation" className="global-menu">
-                 {this.props.menu}
-                </nav>
-              </div>
-              */}
-              <div className="grids col-md-3">
-                {/*<a href="#" className="return-to-link uikit-direction-link uikit-direction-link--left">Return to Client Portal</a>*/}
-                <div className="nexdoc-header-name">
-                  {this.props.name}
-                </div>
+            </Menu>
+          </div>
+          <div className="top-menu-header">
+            <div className="top-links-wrapper">
+              <div className="top-links">
+                <ul>
+                  <li><Link to="/OSS/faces/homePage" className="staff-home-link">Client portal</Link></li>
+                  <li><Link to="/">{this.props.userName}</Link></li>
+                  <li className="header-app-name">{this.props.name}</li>
+                  <li className="logout-li-link-staff"><a href="/auth/faces/logout/">Logout</a></li>
+                </ul>
               </div>
             </div>
-            <div className="row">
-              <div className="grids col-md-20">
-                <nav role="navigation" className="global-menu">
-                 {this.props.menu}
-                </nav>
+          </div>
+
+          <div className="header-block uikit-grid">
+            <div className="container">
+              <div className="row">
+
+                {/*
+                <div className="header-logo grids col-md-9">
+                  <a href="http://www.agriculture.gov.au/">
+                    <img src={logo} className="header-logo-img" alt="Go to home page" aria-hidden="true" />
+                  </a>
+                </div>
+                */}
+
+                {/*
+                <div className="grids col-md-7">
+                  <nav role="navigation" className="global-menu">
+                   {this.props.menu}
+                  </nav>
+                </div>
+                */}
+
+              </div>
+              <div className="row">
+                <div className="grids col-md-20">
+                  <nav role="navigation" className="global-menu">
+                   {this.props.menu}
+                  </nav>
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-     )
+       )
+
+      }
+
    }
 }
 export default Header
