c61f66a7735ef9e445bc3abc1a454b03cbd60d6f
[zapjelly] <[pak.cindy@gmail.com]>
2017-09-29 12:24:12 +1000

    update styles

--- src/index.js
+++ src/index.js
@@ -65,9 +65,9 @@ class Header extends React.Component {
             <div className="top-links">
               <ul>
                 <li><Link to="/" className="staff-home-link">Home</Link></li>
-                <li className="autocomplete-li-link-search"><SearchBar /></li>
-                <li className="header-app-username"><Link to="/">{this.props.userName}</Link></li>
                 <li className="logout-li-link-staff"><a href="/auth/faces/logout/">Logout</a></li>
+                <li className="header-app-username">{this.props.userName}</li>
+                <li className="autocomplete-li-link-search"><SearchBar /></li>
               </ul>
             </div>
             </div>
@@ -97,9 +97,9 @@ class Header extends React.Component {
                 <ul>
                   <li><a href="/OSS/faces/homePage" className="staff-home-link">Client portal</a></li>
                   <li className="header-app-name">{this.props.name}</li>
-                  <li className="autocomplete-li-link-search"><SearchBar /></li>
-                  <li className="header-app-username"><Link to="/">{this.props.userName}</Link></li>
                   <li className="logout-li-link-staff"><a href="/auth/faces/logout/">Logout</a></li>
+                  <li className="header-app-username">{this.props.userName}</li>
+                  <li className="autocomplete-li-link-search"><SearchBar /></li>
                 </ul>
               </div>
             </div>
