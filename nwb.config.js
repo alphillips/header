900d83b4b2a2871b4d82e3d8f4b7b52f49f5080f
[zapjelly] <[pak.cindy@gmail.com]>
2017-10-27 10:54:47 +1100

    add node-sass

--- nwb.config.js
+++ nwb.config.js
@@ -3,5 +3,11 @@ module.exports = {
   npm: {
     esModules: true,
     umd: false
+  },
+  cssPreprocessors: {
+    sass: {
+      test: /\.s[ac]ss$/,
+      loader: require.resolve('sass-loader')
+    }
   }
 }
