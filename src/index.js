7c28d77b7b51b007b2c68316b2f14fd2d35f1ce9
[zapjelly] <[pak.cindy@gmail.com]>
2018-06-04 12:15:09 +1000

    update

--- src/index.js
+++ src/index.js
@@ -191,8 +191,8 @@ class Header extends React.Component {
                 <div className="target-help-content" ref={node => { this.node = node; }}>
                   {this.props.helpPages && this.props.helpPages.length > 0 &&
                   <ul>
-                    {this.props.helpPages.map((helpPage) =>
-                      <li className="help-item">
+                    {this.props.helpPages.map((helpPage, i) =>
+                      <li className="help-item"> key={i}
                         <a href={helpPage.link.indexOf("http")>-1 ? helpPage.link : "#"+helpPage.link}>{helpPage.code}</a>
                       </li>
                     )}
